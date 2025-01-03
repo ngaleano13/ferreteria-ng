51b5920840914b108d66d8843380916ba8281d4e
Nico <nrgaleano00@gmail.com>
2025-01-03 14:03:29 -0300

    Inicio del proyecto

---
+++ src/main/java/com/ngaleano/ferreteria_ng/FerreteriaNgApplication.java
@@ -0,0 +1,13 @@
+package com.ngaleano.ferreteria_ng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FerreteriaNgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FerreteriaNgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ngaleano/ferreteria_ng/FerreteriaNgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ngaleano.ferreteria_ng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FerreteriaNgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
