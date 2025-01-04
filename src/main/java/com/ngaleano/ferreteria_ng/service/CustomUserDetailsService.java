c9f295b1e7240e24b958c9d72892028612950514
Nico <nrgaleano00@gmail.com>
2025-01-04 17:51:02 -0300

    Se termino de agregar los servicios

--- src/main/java/com/ngaleano/ferreteria_ng/service/CustomUserDetailsService.java
+++ src/main/java/com/ngaleano/ferreteria_ng/service/CustomUserDetailsService.java
@@ -1,5 +1,20 @@
 package com.ngaleano.ferreteria_ng.service;
 
+import org.springframework.stereotype.Service;
+
+import com.ngaleano.ferreteria_ng.model.Usuario;
+import com.ngaleano.ferreteria_ng.repository.UsuarioRepository;
+
+@Service
 public class CustomUserDetailsService {
     
+    private UsuarioRepository usuarioRepository;
+
+
+    public Usuario buscarPorId(Long id){
+        return usuarioRepository.findById(id).orElseThrow(
+            () -> new RuntimeException("No se encontro el usuario con el id: "+ id));
+    }
+
+
 }
--- src/main/java/com/ngaleano/ferreteria_ng/service/ProductoService.java
+++ src/main/java/com/ngaleano/ferreteria_ng/service/ProductoService.java
@@ -1,5 +1,44 @@
 package com.ngaleano.ferreteria_ng.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.ngaleano.ferreteria_ng.model.Producto;
+import com.ngaleano.ferreteria_ng.repository.ProductoRepository;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
 public class ProductoService {
     
+    private ProductoRepository productoRepository;
+
+    public Producto obtenerPorId(Long id){
+        return productoRepository.findById(id).orElseThrow(
+            () -> new RuntimeException("No se encontro el producto con el id: "+ id)
+        );
+    }
+
+    public Producto obtenerPorCodigo(String codigo){
+        Producto producto = productoRepository.findByCodProducto(codigo);
+
+        if (producto == null) {
+            throw new RuntimeException("No se encontro el producto con el codigo: " +codigo);
+        }
+        
+        return producto;
+    }
+
+    public List<Producto> obtenerPorNombre(String nombre){
+        return productoRepository.findByNombreProducto(nombre);
+    }
+
+    public List<Producto> listaProductos(){
+        return productoRepository.findAll();
+    }
+
+
+
 }
